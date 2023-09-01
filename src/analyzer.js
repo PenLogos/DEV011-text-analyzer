c42ed5c7e2a44115a008961bf821cde096cb5842
PenLogos <penelopelogos@gmail.com>
2023-09-01 13:02:31 -0500

    añadir js(x2) y html

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,22 +1,84 @@
-const analyzer = {  
+const analyzer = {
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const separarencadenas = text.split(" ")
+    let contarpalabras = 0
+    for (let i = 0; i < separarencadenas.length; i++) {
+      if (separarencadenas[i].trim() !== "") {
+        contarpalabras++;
+      }
+    }
+    return contarpalabras
   },
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    const contarcaracteres = text.length
+    console.log(contarcaracteres)
+    if ((text) =="") {return "0"}
+    else {return contarcaracteres}
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    const separarcaracteres = text.split(" ")
+    const quitarespacios = separarcaracteres.join("")
+    const quitarpuntuacion = quitarespacios
+      .replaceAll(",", "")
+      .replaceAll(".", "")
+      .replaceAll(";", "")
+      .replaceAll(":", "")
+    const contarcaracteressinespacios = quitarpuntuacion.length
+    return contarcaracteressinespacios
+
   },
-  getAverageWordLength: (text) => {    
+  getAverageWordLength: (text) => {
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    function obtenernumpalabras() {
+      const separarcadenas = text.split(" ")
+      let numeropalabras = 0
+      for (let i = 0; i < separarcadenas.length; i++) {
+        if (separarcadenas[i].trim() !== "") {
+          numeropalabras++;
+        }
+      }
+      return numeropalabras
+    }
+    const separarcadenas = text.split(" ")
+    const quitarespacio = separarcadenas.join("")
+    const largopalabras = quitarespacio.length
+    const resultado = obtenernumpalabras()
+    const promedio = largopalabras / resultado
+    const limitardecimales = promedio.toFixed(2)
+    if (text) {
+      return limitardecimales
+    }
+    else {
+      return "0"
+    }
   },
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    const separar = text.split(" ")
+    let contarnumeros = 0
+    for (let i = 0; i < separar.length; i++) {
+      const numeroscadena = parseInt(separar[i])
+      if (!isNaN(numeroscadena)) {
+        contarnumeros++;
+      }
+    }
+    return contarnumeros
   },
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    const cadenas = text.split(" ")
+    let sumanumeros = 0.0
+    for (let i = 0; i < cadenas.length; i++) {
+      const solonumeros = parseFloat(cadenas[i])
+      if (!isNaN(solonumeros)) {
+        sumanumeros += solonumeros
+      }
+    }
+    return sumanumeros
   },
 };
-
 export default analyzer;
+
